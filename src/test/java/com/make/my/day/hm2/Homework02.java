3a6b74720bd8eb8ef684c7e6026cddc94d2cf1c6
Roman Kotov <roman_kotov@epam.com>
2019-03-20 00:57:26 +0300

    RKotov. 2 homeword is done.

--- src/test/java/com/make/my/day/hm2/Homework02.java
+++ src/test/java/com/make/my/day/hm2/Homework02.java
@@ -15,258 +15,265 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+
 import org.junit.Test;
 
 public class Homework02 {
 
-  @Test
-  public void concatenateChars() {
-    //create your realization with lambda
-    Function<Character[], String> charConcatenator = chars -> {
-      StringBuilder sb = new StringBuilder();
-      for (char c : chars) {
-        sb.append(c);
-      }
-      return sb.toString();
-    };
+    @Test
+    public void concatenateChars() {
+        //create your realization with lambda
+        Function<Character[], String> charConcatenator = chars -> {
+            StringBuilder sb = new StringBuilder();
+            for (char c : chars) {
+                sb.append(c);
+            }
+            return sb.toString();
+        };
 
-    String result_1 = charConcatenator.apply(new Character[]{'a', 'b', 'c'});
-    String result_2 = charConcatenator.apply(new Character[]{'H', 'e', 'l', 'l', 'o'});
-    String result_3 = charConcatenator.apply(new Character[]{'T', 'u', 'r', 't', 'l', 'e'});
+        String result_1 = charConcatenator.apply(new Character[]{'a', 'b', 'c'});
+        String result_2 = charConcatenator.apply(new Character[]{'H', 'e', 'l', 'l', 'o'});
+        String result_3 = charConcatenator.apply(new Character[]{'T', 'u', 'r', 't', 'l', 'e'});
 
-    assertEquals("abc", result_1);
-    assertEquals("Hello", result_2);
-    assertEquals("Turtle", result_3);
-  }
-
-  @Test
-  public void reversedWord() {
-    // create your realization with lambda
-    Predicate<String> isReversedStringTheSame = s -> new StringBuilder(s).reverse().toString().equals(s);
-
-    boolean result_1 = isReversedStringTheSame.test("abccba");
-    boolean result_2 = isReversedStringTheSame.test("level");
-    boolean result_3 = isReversedStringTheSame.test("cow");
-    boolean result_4 = isReversedStringTheSame.test("radar");
-    boolean result_5 = isReversedStringTheSame.test("mellow");
-    boolean result_6 = isReversedStringTheSame.test("madam");
-
-    assertTrue(result_1);
-    assertTrue(result_2);
-    assertFalse(result_3);
-    assertTrue(result_4);
-    assertFalse(result_5);
-    assertTrue(result_6);
-  }
-
-  private class Counter {
-
-    private Function<String, Integer> transform;
-    private BinaryOperator<Integer> summarizer;
-
-    public Counter(Function<String, Integer> transform,
-        BinaryOperator<Integer> summarizer) {
-      this.transform = transform;
-      this.summarizer = summarizer;
+        assertEquals("abc", result_1);
+        assertEquals("Hello", result_2);
+        assertEquals("Turtle", result_3);
     }
 
-    public int getSum(String firstInput, String secondInput) {
-      int firstTransformedNumber = transform.apply(firstInput);
-      int secondTransformedNumber = transform.apply(secondInput);
-      return summarizer.apply(firstTransformedNumber, secondTransformedNumber);
-    }
-  }
+    @Test
+    public void reversedWord() {
+        // create your realization with lambda
+        Predicate<String> isReversedStringTheSame = s -> new StringBuilder(s).reverse().toString().equals(s);
 
-  @Test
-  public void transformAndProvideSumWithCounter() {
-    // create your realization with lambda
-    Function<String, Integer> transform = Integer::valueOf;
+        boolean result_1 = isReversedStringTheSame.test("abccba");
+        boolean result_2 = isReversedStringTheSame.test("level");
+        boolean result_3 = isReversedStringTheSame.test("cow");
+        boolean result_4 = isReversedStringTheSame.test("radar");
+        boolean result_5 = isReversedStringTheSame.test("mellow");
+        boolean result_6 = isReversedStringTheSame.test("madam");
 
-    // create your realization with lambda
-    BinaryOperator<Integer> increment = (integer, integer2) -> integer + integer2;
-
-    Counter sut_1 = new Counter(transform, increment);
-    Counter sut_2 = new Counter(transform, increment);
-    Counter sut_3 = new Counter(transform, increment);
-
-    assertEquals(5, sut_1.getSum("3", "2"));
-    assertEquals(10, sut_2.getSum("5", "5"));
-    assertEquals(57, sut_3.getSum("24", "33"));
-  }
-
-  @Test
-  public void consumerPrintInPrintStream() {
-    final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    final PrintStream original = System.out;
-    System.setOut(new PrintStream(outContent));
-
-    // method must print parameters in System.out
-    Consumer<Object> printHelloInSystemOut = System.out::print;
-
-    printHelloInSystemOut.accept("hello");
-    assertEquals("hello", outContent.toString());
-
-    outContent.reset();
-
-    printHelloInSystemOut.accept("epam");
-    assertEquals("epam", outContent.toString());
-
-    outContent.reset();
-
-    printHelloInSystemOut.accept(12345);
-    assertEquals("12345", outContent.toString());
-
-    System.setOut(original);
-  }
-
-  @Test
-  public void testPredicateScenario() {
-
-    // create your realization with lambda
-    Predicate<String> isStartsWithXX = s -> s.startsWith("XX");
-
-    // create your realization with lambda
-    Predicate<String> isEndWithZZZ = isStartsWithXX.and(s -> s.endsWith("ZZZ"));
-
-    // create your realization with lambda
-    Predicate<String> haveFiveStarsInRow = isEndWithZZZ.or(s -> s.contains("*****"));
-
-    assertFalse(haveFiveStarsInRow.test("Xnot_rightZZZ"));
-    assertTrue(haveFiveStarsInRow.test("XXsuperduperZZZ"));
-    assertFalse(haveFiveStarsInRow.test("XXnotrighttooZZ"));
-    assertTrue(haveFiveStarsInRow.test("one*two**three***four****five*****"));
-    assertFalse(haveFiveStarsInRow.test("this**will**fail*"));
-  }
-
-  @Test
-  public void chainOfFunctionInvocation() {
-    List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-
-    // create realization with lambda
-    Predicate<Integer> isEven = integer -> integer % 2 == 0;
-
-    List<Integer> evenNumbersList = new ArrayList<>();
-
-    for (Integer num : numbers) {
-      if (isEven.test(num)) {
-        evenNumbersList.add(num);
-      }
+        assertTrue(result_1);
+        assertTrue(result_2);
+        assertFalse(result_3);
+        assertTrue(result_4);
+        assertFalse(result_5);
+        assertTrue(result_6);
     }
 
-    assertTrue(evenNumbersList.containsAll(Arrays.asList(2, 4, 6, 8, 10)));
+    private class Counter {
 
-    // create realization with lambda
-    Function<List<Integer>, List<Integer>> sumWithNextElement = integers -> {
-      List<Integer> result = new ArrayList<>(integers.size());
-      for (int index = 0; index < integers.size() - 1; index++) {
-        result.add(integers.get(index) + integers.get(index + 1));
-      }
-      result.add(integers.get(integers.size() - 1) + integers.get(0));
-      return result;
-    };
+        private Function<String, Integer> transform;
+        private BinaryOperator<Integer> summarizer;
 
-    List<Integer> newNumbers = sumWithNextElement.apply(evenNumbersList);
+        public Counter(Function<String, Integer> transform,
+                       BinaryOperator<Integer> summarizer) {
+            this.transform = transform;
+            this.summarizer = summarizer;
+        }
 
-    assertTrue(newNumbers.containsAll(Arrays.asList(6, 10, 14, 18, 12)));
-
-    // create realization with lambda
-    Predicate<Integer> moreThanThirteen = integer -> integer >= 13;
-
-    List<Integer> numbersMoreThanFifteen = new ArrayList<>();
-
-    for (Integer num : newNumbers) {
-      if (moreThanThirteen.test(num)) {
-        numbersMoreThanFifteen.add(num);
-      }
+        public int getSum(String firstInput, String secondInput) {
+            int firstTransformedNumber = transform.apply(firstInput);
+            int secondTransformedNumber = transform.apply(secondInput);
+            return summarizer.apply(firstTransformedNumber, secondTransformedNumber);
+        }
     }
 
-    assertTrue(numbersMoreThanFifteen.containsAll(Arrays.asList(14,18)));
-  }
+    @Test
+    public void transformAndProvideSumWithCounter() {
+        // create your realization with lambda
+        Function<String, Integer> transform = Integer::valueOf;
 
-  //TODO: implement predicate with lambda
-  private static Predicate<String> lengthMoreThanSeven() {
-    return null;
-  }
+        // create your realization with lambda
+        BinaryOperator<Integer> increment = (integer, integer2) -> integer + integer2;
 
-  //TODO: implement, if predicate correct "str" => "strstr" if not "str..."
-  private static Function<Predicate<String>, Function<String, String>> doubleStringOrAddThreeDots() {
-    return null;
-  }
+        Counter sut_1 = new Counter(transform, increment);
+        Counter sut_2 = new Counter(transform, increment);
+        Counter sut_3 = new Counter(transform, increment);
 
-  //TODO: implement, return word length from function
-  private static Function<String, Integer> lengthOfWord(
-      Function<Predicate<String>, Function<String, String>> doubledOrWithThreeDots) {
-    return null;
-  }
-
-  @Test
-  public void checkDeepCallBack() {
-    assertFalse(lengthMoreThanSeven().test("no"));
-    assertTrue(lengthMoreThanSeven().test("wonderful"));
-    assertFalse(lengthMoreThanSeven().test("welcome"));
-
-    assertEquals("no...", doubleStringOrAddThreeDots()
-        .apply(lengthMoreThanSeven()).apply("no"));
-
-    assertEquals("wonderfulwonderful", doubleStringOrAddThreeDots()
-        .apply(lengthMoreThanSeven()).apply("wonderful"));
-
-    assertEquals("welcome...", doubleStringOrAddThreeDots()
-        .apply(lengthMoreThanSeven()).apply("welcome"));
-
-    assertEquals(5, lengthOfWord(doubleStringOrAddThreeDots()).apply("no").intValue());
-    assertEquals(18, lengthOfWord(doubleStringOrAddThreeDots()).apply("wonderful").intValue());
-    assertEquals(10, lengthOfWord(doubleStringOrAddThreeDots()).apply("welcome").intValue());
-  }
-
-  private class LazyProperty {
-
-    private Supplier<Integer> supplier;
-    private Integer lazy;
-
-    public LazyProperty(Supplier<Integer> supplier) {
-      this.supplier = supplier;
+        assertEquals(5, sut_1.getSum("3", "2"));
+        assertEquals(10, sut_2.getSum("5", "5"));
+        assertEquals(57, sut_3.getSum("24", "33"));
     }
 
-    //TODO: initialize "lazy" using "supplier" only one time
-    public int getLazy() {
-      return 0;
+    @Test
+    public void consumerPrintInPrintStream() {
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        final PrintStream original = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        // method must print parameters in System.out
+        Consumer<Object> printHelloInSystemOut = System.out::print;
+
+        printHelloInSystemOut.accept("hello");
+        assertEquals("hello", outContent.toString());
+
+        outContent.reset();
+
+        printHelloInSystemOut.accept("epam");
+        assertEquals("epam", outContent.toString());
+
+        outContent.reset();
+
+        printHelloInSystemOut.accept(12345);
+        assertEquals("12345", outContent.toString());
+
+        System.setOut(original);
     }
-  }
 
-  @Test
-  public void lazyLoading() {
+    @Test
+    public void testPredicateScenario() {
 
-    //TODO: provide supplier in constructor with lambda
-    LazyProperty sut = new LazyProperty(null);
-    LazyProperty sut_2 = new LazyProperty(null);
-    LazyProperty sut_3 = new LazyProperty(null);
+        // create your realization with lambda
+        Predicate<String> isStartsWithXX = s -> s.startsWith("XX");
 
-    assertNull(sut.lazy);
-    assertNull(sut_2.lazy);
-    assertNull(sut_3.lazy);
+        // create your realization with lambda
+        Predicate<String> isEndWithZZZ = isStartsWithXX.and(s -> s.endsWith("ZZZ"));
 
-    assertEquals(1, sut.getLazy());
-    assertEquals(2, sut_2.getLazy());
-    assertEquals(3, sut_3.getLazy());
+        // create your realization with lambda
+        Predicate<String> haveFiveStarsInRow = isEndWithZZZ.or(s -> s.contains("*****"));
 
-    assertEquals(1, sut.lazy.intValue());
-    assertEquals(2, sut_2.lazy.intValue());
-    assertEquals(3, sut_3.lazy.intValue());
-  }
+        assertFalse(haveFiveStarsInRow.test("Xnot_rightZZZ"));
+        assertTrue(haveFiveStarsInRow.test("XXsuperduperZZZ"));
+        assertFalse(haveFiveStarsInRow.test("XXnotrighttooZZ"));
+        assertTrue(haveFiveStarsInRow.test("one*two**three***four****five*****"));
+        assertFalse(haveFiveStarsInRow.test("this**will**fail*"));
+    }
 
-  @Test
-  public void andThenTest() {
-    //TODO: realize with lambda
-    Function<Integer, Integer> sumIntegerOnSix = null;
-    Function<Integer, Integer> thenMinusThree = null;
-    Function<Integer, Integer> afterMultipleOnFive = null;
+    @Test
+    public void chainOfFunctionInvocation() {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
-    Integer result = sumIntegerOnSix
-        .andThen(thenMinusThree)
-        .andThen(afterMultipleOnFive)
-        .apply(10);
+        // create realization with lambda
+        Predicate<Integer> isEven = integer -> integer % 2 == 0;
 
-    assertEquals(65, result.intValue());
-  }
+        List<Integer> evenNumbersList = new ArrayList<>();
+
+        for (Integer num : numbers) {
+            if (isEven.test(num)) {
+                evenNumbersList.add(num);
+            }
+        }
+
+        assertTrue(evenNumbersList.containsAll(Arrays.asList(2, 4, 6, 8, 10)));
+
+        // create realization with lambda
+        Function<List<Integer>, List<Integer>> sumWithNextElement = integers -> {
+            List<Integer> result = new ArrayList<>(integers.size());
+            for (int index = 0; index < integers.size() - 1; index++) {
+                result.add(integers.get(index) + integers.get(index + 1));
+            }
+            result.add(integers.get(integers.size() - 1) + integers.get(0));
+            return result;
+        };
+
+        List<Integer> newNumbers = sumWithNextElement.apply(evenNumbersList);
+
+        assertTrue(newNumbers.containsAll(Arrays.asList(6, 10, 14, 18, 12)));
+
+        // create realization with lambda
+        Predicate<Integer> moreThanThirteen = integer -> integer >= 13;
+
+        List<Integer> numbersMoreThanFifteen = new ArrayList<>();
+
+        for (Integer num : newNumbers) {
+            if (moreThanThirteen.test(num)) {
+                numbersMoreThanFifteen.add(num);
+            }
+        }
+
+        assertTrue(numbersMoreThanFifteen.containsAll(Arrays.asList(14, 18)));
+    }
+
+    // implement predicate with lambda
+    private static Predicate<String> lengthMoreThanSeven() {
+        return s -> s.length() > 7;
+    }
+
+    // implement, if predicate correct "str" => "strstr" if not "str..."
+    private static Function<Predicate<String>, Function<String, String>> doubleStringOrAddThreeDots() {
+        return (stringPredicate) ->
+                (s) -> stringPredicate.test(s) ? s + s : s + "...";
+    }
+
+
+    // implement, return word length from function
+    private static Function<String, Integer> lengthOfWord(
+            Function<Predicate<String>, Function<String, String>> doubledOrWithThreeDots) {
+        return s -> doubledOrWithThreeDots.apply(lengthMoreThanSeven()).apply(s).length();
+    }
+
+    @Test
+    public void checkDeepCallBack() {
+
+        assertFalse(lengthMoreThanSeven().test("no"));
+        assertTrue(lengthMoreThanSeven().test("wonderful"));
+        assertFalse(lengthMoreThanSeven().test("welcome"));
+
+        assertEquals("no...", doubleStringOrAddThreeDots()
+                .apply(lengthMoreThanSeven()).apply("no"));
+
+        assertEquals("wonderfulwonderful", doubleStringOrAddThreeDots()
+                .apply(lengthMoreThanSeven()).apply("wonderful"));
+
+        assertEquals("welcome...", doubleStringOrAddThreeDots()
+                .apply(lengthMoreThanSeven()).apply("welcome"));
+
+        assertEquals(5, lengthOfWord(doubleStringOrAddThreeDots()).apply("no").intValue());
+        assertEquals(18, lengthOfWord(doubleStringOrAddThreeDots()).apply("wonderful").intValue());
+        assertEquals(10, lengthOfWord(doubleStringOrAddThreeDots()).apply("welcome").intValue());
+    }
+
+    private class LazyProperty {
+
+        private Supplier<Integer> supplier;
+        private Integer lazy;
+
+        public LazyProperty(Supplier<Integer> supplier) {
+            this.supplier = supplier;
+        }
+
+        //initialize "lazy" using "supplier" only one time
+        public int getLazy() {
+            if (lazy == null) {
+                lazy = supplier.get();
+            }
+            return lazy;
+        }
+    }
+
+    @Test
+    public void lazyLoading() {
+
+        // provide supplier in constructor with lambda
+        LazyProperty sut = new LazyProperty(() -> 1);
+        LazyProperty sut_2 = new LazyProperty(() -> 2);
+        LazyProperty sut_3 = new LazyProperty(() -> 3);
+
+        assertNull(sut.lazy);
+        assertNull(sut_2.lazy);
+        assertNull(sut_3.lazy);
+
+        assertEquals(1, sut.getLazy());
+        assertEquals(2, sut_2.getLazy());
+        assertEquals(3, sut_3.getLazy());
+
+        assertEquals(1, sut.lazy.intValue());
+        assertEquals(2, sut_2.lazy.intValue());
+        assertEquals(3, sut_3.lazy.intValue());
+    }
+
+    @Test
+    public void andThenTest() {
+        // realize with lambda
+        Function<Integer, Integer> sumIntegerOnSix = integer -> integer + 6;
+        Function<Integer, Integer> thenMinusThree = integer -> integer - 3;
+        Function<Integer, Integer> afterMultipleOnFive = integer -> integer * 5;
+
+        Integer result = sumIntegerOnSix
+                .andThen(thenMinusThree)
+                .andThen(afterMultipleOnFive)
+                .apply(10);
+
+        assertEquals(65, result.intValue());
+    }
 }
